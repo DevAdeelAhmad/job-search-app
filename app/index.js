2474c9437cb2e23a7f38d4ff2f4d5e928fde95e0
DevAdeelAhmad <devadeelahmad@gmail.com>
2023-09-23 07:08:28 +0500

    Basic Setup

---
+++ app/index.js
@@ -0,0 +1,11 @@
+import {View, Text} from 'react-native';
+
+export default function Home (){
+    return(
+        <>
+        <View>
+            <Text>Home</Text>
+        </View>
+        </>
+    );
+}
