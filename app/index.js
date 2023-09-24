7174db474c24d86ed6ce8b07acb48a1dc70b1fe4
DevAdeelAhmad <devadeelahmad@gmail.com>
2023-09-24 22:34:02 +0500

    Assets added

--- app/index.js
+++ app/index.js
@@ -3,7 +3,7 @@ import {View, Text} from 'react-native';
 export default function Home (){
     return(
         <>
-        <View>
+        <View> 
             <Text>Home</Text>
         </View>
         </>
